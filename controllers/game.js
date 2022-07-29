d9b732d4e6a9920075ab08a65e2cb6ae3f2fd55c
swaff-y <kyle@swaff.id.au>
2022-07-29 23:08:57 +1000

    user to User bug

--- controllers/game.js
+++ controllers/game.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Game = require("../models/game");
+const Game = require("../models/Game");
 const log = require("../logger");
 
 //Get All
